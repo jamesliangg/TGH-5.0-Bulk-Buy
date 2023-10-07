8a1bf5073a7f5a75a772205f1ebdebb334177ad9
James Liang <86000097+jamesliangg@users.noreply.github.com>
2023-10-07 17:39:09 -0400

    Redis commit

--- backend/index.js
+++ backend/index.js
@@ -1,5 +1,5 @@
 import {mongoQueryOne, mongoInsertOne, mongoQueryMultiple, mongoUpdateOne} from "./mongo.js";
-import {redis_set, redis_get} from "./redisdb.js";
+import {redis_get, redis_hSet, redis_hGet} from "./redisdb.js";
 import express from 'express';
 const app = express();
 import bodyParser from "body-parser";
@@ -70,12 +70,12 @@ app.post('/api/auth', async function (req, res) {
             try {
                 const uid = req.body.uid;
                 const pwd = req.body.pwd;
-                result = await redis_get(uid);
+                result = await redis_hGet(uid, "password");
                 if (result == pwd) {
-                    result = "Authenticated";
+                    result = await redis_hGet(uid, "name");
                 }
                 else {
-                    result = "Invalid";
+                    result = "ERROR: Invalid";
                 }
             }
             catch(err) {
@@ -86,7 +86,54 @@ app.post('/api/auth', async function (req, res) {
             try {
                 const uid = req.body.uid;
                 const pwd = req.body.pwd;
-                result = await redis_set(uid, pwd);
+                const value = {
+                    pwd: pwd,
+                    name: req.body.name
+                }
+                console.log(value);
+                result = await redis_hSet(uid, value);
+            }
+            catch(err) {
+                console.log(err.message);
+            }
+            break;
+    }
+
+    response = JSON.stringify({
+        "result": result
+    });
+    console.log(response);
+    res.send(response);
+});
+
+app.post('/api/ingredients', async function (req, res) {
+    const data = req.body;
+    const action = req.body.action;
+    console.log(req.ip);
+    let today = new Date();
+    let date = today.getFullYear()+'-'+(today.getMonth()+1)+'-'+today.getDate();
+    let time = today.getHours() + ":" + today.getMinutes() + ":" + today.getSeconds();
+    let dateTime = date+' '+time;
+    console.log(dateTime);
+    console.log('Received data: ', data);
+    let response = "No action received.";
+    let result = "";
+    switch(action) {
+        case("get"):
+            try {
+                const uid = req.body.uid;
+                const field = req.body.field;
+                result = await redis_get(uid, field);
+            }
+            catch(err) {
+                console.log(err.message);
+            }
+            break;
+        case("set"):
+            try {
+                const uid = req.body.uid;
+                const info = req.body.info;
+                result = await redis_set(uid, info);
             }
             catch(err) {
                 console.log(err.message);
--- backend/redisdb.js
+++ backend/redisdb.js
@@ -5,6 +5,7 @@ const password = process.env.REDIS_PASSWORD;
 const host = process.env.REDIS_HOST;
 const port = process.env.REDIS_PORT;
 
+// https://redis.io/commands/get/
 export async function redis_get(key) {
     const client = createClient({
         password: password,
@@ -19,6 +20,7 @@ export async function redis_get(key) {
     return value;
 }
 
+// https://redis.io/commands/set/
 export async function redis_set(key, value) {
     const client = await createClient({
         password: password,
@@ -29,6 +31,42 @@ export async function redis_set(key, value) {
     });
     await client.connect();
     const response = await client.set(key, value);
+    await client.disconnect();
+    return response;
+}
+
+// https://redis.io/commands/hget/
+export async function redis_hGet(key, field) {
+    const client = createClient({
+        password: password,
+        socket: {
+            host: host,
+            port: port
+        }
+    });
+    await client.connect();
+    const value = await client.hGet(key, field);
+    await client.disconnect();
+    return value;
+}
+
+// https://redis.io/commands/hset/
+export async function redis_hSet(key, value) {
+    const client = await createClient({
+        password: password,
+        socket: {
+            host: host,
+            port: port
+        }
+    });
+    await client.connect();
+    const response = await client.hSet(key, value);
+    // const response = await client.hSet('user-session:123', {
+    //     name: 'John',
+    //     surname: 'Smith',
+    //     company: 'Redis',
+    //     age: 29
+    // });
     await client.disconnect();
     return response;
 }

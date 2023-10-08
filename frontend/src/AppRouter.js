f05e7f4ccc5b370ee600057959851c8204d7301f
Krishna Cheemalalapati <krishkrishct@gmail.com>
2023-10-07 23:25:03 -0400

    git

---
+++ frontend/src/AppRouter.js
@@ -0,0 +1,29 @@
+import React from 'react'
+import { BrowserRouter, Route, Routes } from 'react-router-dom'
+import Dashboard from './pages/Dashboard/Dashboard'
+import LandingPage from './pages/LandingPage/LandingPage'
+import LoginPage from './pages/Login/LoginPage'
+import About from './pages/About/About'
+import Privacy from './pages/Privacy/Privacy'
+
+
+class AppRouter extends React.Component{
+  render(){
+    return(
+      <BrowserRouter>
+        <div>
+          <Routes>
+            <Route path = "/" element = {<LandingPage />} exact = {true} />
+            <Route path = "/landingpage" element = {<LandingPage />} exact = {true} />
+            <Route path = "/dashboard" element = {<Dashboard />} exact = {true} />
+            <Route path = "/login" element = {<LoginPage />} exact = {true} />
+            <Route path = "/about" element = {<About />} exact = {true} />
+            <Route path = "/privacy" element = {<Privacy />} exact = {true} />
+          </Routes>
+        </div>
+      </BrowserRouter>
+    )
+  }
+}
+
+export default AppRouter
---
+++ frontend/src/index.js
@@ -0,0 +1,22 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import { Provider } from "react-redux";
+import { PersistGate } from 'redux-persist/integration/react'
+import { persistStore } from 'redux-persist'
+
+import AppRouter from './AppRouter';
+import store from "./redux/configureStore"
+import "./styles/App.css"
+//  import 'bootstrap/dist/css/bootstrap.min.css';
+
+
+const persistor = persistStore(store)
+
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(
+  <Provider store={store}>
+    <PersistGate loading={null} persistor={persistor}>
+      <AppRouter />
+    </PersistGate>
+  </Provider>
+);

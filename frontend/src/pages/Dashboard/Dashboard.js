b5b9cfa5d3c4f002c50c8cadce2c82e5b2a8ef96
Krishna Cheemalalapati <krishkrishct@gmail.com>
2023-10-08 02:30:50 -0400

    up

---
+++ frontend/src/pages/Dashboard/Dashboard.js
@@ -0,0 +1,153 @@
+import React from 'react';
+import Products from './Products';
+
+const productsData = [
+  {
+    _id: 1,
+    image: 'product1.jpg',
+    title: 'Product 1',
+    description: 'Description of Product 1',
+    price: 19.99,
+    imageUrl: 'https://example.com/product1.jpg',
+  },
+  {
+    _id: 1,
+    image: 'product1.jpg',
+    title: 'Product 1',
+    description: 'Description of Product 1',
+    price: 19.99,
+    imageUrl: 'https://example.com/product1.jpg',
+  },
+  {
+    _id: 1,
+    image: 'product1.jpg',
+    title: 'Product 1',
+    description: 'Description of Product 1',
+    price: 19.99,
+    imageUrl: 'https://example.com/product1.jpg',
+  },
+  {
+    _id: 1,
+    image: 'product1.jpg',
+    title: 'Product 1',
+    description: 'Description of Product 1',
+    price: 19.99,
+    imageUrl: 'https://example.com/product1.jpg',
+  },
+  {
+    _id: 1,
+    image: 'product1.jpg',
+    title: 'Product 1',
+    description: 'Description of Product 1',
+    price: 19.99,
+    imageUrl: 'https://example.com/product1.jpg',
+  },
+  {
+    _id: 1,
+    image: 'product1.jpg',
+    title: 'Product 1',
+    description: 'Description of Product 1',
+    price: 19.99,
+    imageUrl: 'https://example.com/product1.jpg',
+  },
+  {
+    _id: 1,
+    image: 'product1.jpg',
+    title: 'Product 1',
+    description: 'Description of Product 1',
+    price: 19.99,
+    imageUrl: 'https://example.com/product1.jpg',
+  },
+  {
+    _id: 1,
+    image: 'product1.jpg',
+    title: 'Product 1',
+    description: 'Description of Product 1',
+    price: 19.99,
+    imageUrl: 'https://example.com/product1.jpg',
+  },
+  {
+    _id: 1,
+    image: 'product1.jpg',
+    title: 'Product 1',
+    description: 'Description of Product 1',
+    price: 19.99,
+    imageUrl: 'https://example.com/product1.jpg',
+  },
+  {
+    _id: 1,
+    image: 'product1.jpg',
+    title: 'Product 1',
+    description: 'Description of Product 1',
+    price: 19.99,
+    imageUrl: 'https://example.com/product1.jpg',
+  },
+  {
+    _id: 1,
+    image: 'product1.jpg',
+    title: 'Product 1',
+    description: 'Description of Product 1',
+    price: 19.99,
+    imageUrl: 'https://example.com/product1.jpg',
+  },
+  {
+    _id: 1,
+    image: 'product1.jpg',
+    title: 'Product 1',
+    description: 'Description of Product 1',
+    price: 19.99,
+    imageUrl: 'https://example.com/product1.jpg',
+  },
+  {
+    _id: 1,
+    image: 'product1.jpg',
+    title: 'Product 1',
+    description: 'Description of Product 1',
+    price: 19.99,
+    imageUrl: 'https://example.com/product1.jpg',
+  },
+  {
+    _id: 1,
+    image: 'product1.jpg',
+    title: 'Product 1',
+    description: 'Description of Product 1',
+    price: 19.99,
+    imageUrl: 'https://example.com/product1.jpg',
+  },
+  {
+    _id: 1,
+    image: 'product1.jpg',
+    title: 'Product 1',
+    description: 'Description of Product 1',
+    price: 19.99,
+    imageUrl: 'https://example.com/product1.jpg',
+  },
+  {
+    _id: 1,
+    image: 'product1.jpg',
+    title: 'Product 1',
+    description: 'Description of Product 1',
+    price: 19.99,
+    imageUrl: 'https://example.com/product1.jpg',
+  },
+  {
+    _id: 1,
+    image: 'product1.jpg',
+    title: 'Product 1',
+    description: 'Description of Product 1',
+    price: 19.99,
+    imageUrl: 'https://example.com/product1.jpg',
+  },
+  // Add more product objects here...
+];
+
+function Dashboard() {
+  return (
+    <div>
+      <h1>Product List</h1>
+      <Products products={productsData} />
+    </div>
+  );
+}
+
+export default Dashboard;
